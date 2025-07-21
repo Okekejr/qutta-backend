ebfd07492902bf94a1e7863cf32666f52bb00830
Emmanuel <dylanokeks@icloud.com>
2025-07-20 18:35:23 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => res.send("Qutta API Running"));
+
+server.listen(PORT, () => {
+  console.log(`Server + WebSocket running on http://localhost:${PORT}`);
+});
